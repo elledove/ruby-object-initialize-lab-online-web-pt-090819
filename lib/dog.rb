e8cb98649b87cd5eefc32a7ad91d4747deeca26d
Lauren Dove <elledoveinc@gmail.com>
2019-09-26 02:32:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
-  def initialize(name , breed)
+  def initialize(name)
     @name = name
-    @breed = breed
+    #@breed = breed
   end
   def name=(name)
     @name = name 
@@ -9,10 +9,10 @@ class Dog
   def name
     @name 
   end
-  def breed=(breed)
-    @breed = "Mutt"
-  end
-  def breed 
-    @breed
-  end
+  #def breed=(breed)
+    #@breed = "Mutt"
+  #end
+  #def breed 
+   # @breed
+  #end
 end
