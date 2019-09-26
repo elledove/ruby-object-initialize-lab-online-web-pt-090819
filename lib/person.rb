1fc8af051586bbad7ac41763287cfa71c775c4ab
Lauren Dove <elledoveinc@gmail.com>
2019-09-26 02:11:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -10,21 +10,3 @@ class Person
   end
 end
 
-class Dog 
-  def initialize(name , breed)
-    @name = name
-    @breed = breed
-  end
-  def name=(name)
-    @name = name 
-  end 
-  def name
-    @name 
-  end
-  def breed=(breed)
-    @breed = "Mutt"
-  end
-  def breed 
-    @breed
-  end
-end
